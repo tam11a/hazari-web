cc13ca436875f8a61ef46d26032797eff966e38a
tam11a <ibrahimsadiktamim@gmail.com>
2025-02-01 08:46:44 +0600

    added: create table

--- src/app/(welcome)/create/page.tsx
+++ src/app/(welcome)/create/page.tsx
@@ -1,23 +1,51 @@
 "use client";
 
+import instance from "@/app/axios-instance";
 import { Button, Card, TextField, Typography } from "@mui/material";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export default function Create() {
   const [name, setName] = useState<string>("");
+  const [username, setUsername] = useState<string>("");
   const [target, setTarget] = useState<number>(1000);
   const router = useRouter();
+
+  const createTable = async () => {
+    const res = await instance.post("/create-table", {
+      name,
+      username,
+      target,
+    });
+    if (res.status === 201) {
+      console.log(res.data);
+      localStorage.setItem("username", res.data.data.party_owner.name);
+      localStorage.setItem("user_id", res.data.data.party_owner.id);
+      router.push(`/game/${res.data.data.id}`);
+    }
+  };
+
   return (
     <Card className="!bg-green-100 max-w-md rounded-md p-4 mx-auto flex flex-col items-center space-y-4">
       <Typography variant="button">CREATE NEW TABLE</Typography>
       <TextField
-        label="Username"
+        label="Table Name"
         size="small"
         variant="outlined"
         value={name}
         onChange={(e) => {
           setName(e.target.value);
+        }}
+        fullWidth
+        required
+      />
+      <TextField
+        label="Username"
+        size="small"
+        variant="outlined"
+        value={username}
+        onChange={(e) => {
+          setUsername(e.target.value);
         }}
         fullWidth
         required
@@ -38,9 +66,8 @@ export default function Create() {
         variant="contained"
         color="primary"
         onClick={() => {
-          if (name) {
-            localStorage.setItem("username", name);
-            router.replace("/game/1");
+          if (name && username) {
+            createTable();
           }
         }}
       >
